a498cf0c8c27bf67a3e09bc4c1e6e08d7c76b76f
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-11-18 07:45:38 -0500

    Refactoring

--- processors/src/main/java/org/nasdanika/models/source/processors/ecore/EcoreGenSourceProcessorsFactory.java
+++ processors/src/main/java/org/nasdanika/models/source/processors/ecore/EcoreGenSourceProcessorsFactory.java
@@ -2,6 +2,7 @@ package org.nasdanika.models.source.processors.ecore;
 
 import java.util.function.BiConsumer;
 
+import org.eclipse.emf.ecore.EObject;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.common.Reflector.Factory;
@@ -50,7 +51,7 @@ public class EcoreGenSourceProcessorsFactory {
 	)
 	public EPackageNodeProcessor createEPackageProcessor(
 			NodeProcessorConfig<WidgetFactory, WidgetFactory> config, 
-			java.util.function.Function<ProgressMonitor, Action> prototypeProvider,
+			java.util.function.BiFunction<EObject, ProgressMonitor, Action> prototypeProvider,
 			BiConsumer<Label, ProgressMonitor> labelConfigurator,
 			ProgressMonitor progressMonitor) {		
 		return new EPackageNodeProcessor(config, context, prototypeProvider) {
